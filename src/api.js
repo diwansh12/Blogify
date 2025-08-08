3dc8f7b3d003502eee5310c8588b01d915d93e5d
diwansh12 <diwansh1112@gmail.com>
2025-08-09 00:39:43 +0530

    updated blogify

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://blogify-bupb.vercel.app/", 
 });
 
 api.interceptors.request.use((config) => {
